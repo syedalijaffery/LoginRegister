0f4d199d896b9dfeb18271e63c7969c837b90ad9
Syed Ali Jaffery <syedalijaffry@gmail.com>
2018-08-18 12:58:11 +0500

    Initial commit

---
+++ app/src/main/java/com/androidtutorialshub/loginregister/activities/UsersListActivity.java
@@ -0,0 +1,73 @@
+package com.androidtutorialshub.loginregister.activities;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.androidtutorialshub.loginregister.R;
+import com.androidtutorialshub.loginregister.adapters.UsersRecyclerAdapter;
+import com.androidtutorialshub.loginregister.model.User;
+import com.androidtutorialshub.loginregister.sql.DatabaseHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lalit on 10/10/2016.
+ */
+
+public class UsersListActivity extends AppCompatActivity {
+
+    private AppCompatActivity activity = UsersListActivity.this;
+    private AppCompatTextView textViewName;
+    private RecyclerView recyclerViewUsers;
+    private List<User> listUsers;
+    private UsersRecyclerAdapter usersRecyclerAdapter;
+    private DatabaseHelper databaseHelper;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users_list);
+        getSupportActionBar().setTitle("");
+
+        textViewName = (AppCompatTextView) findViewById(R.id.textViewName);
+        recyclerViewUsers = (RecyclerView) findViewById(R.id.recyclerViewUsers);
+
+
+        listUsers = new ArrayList<>();
+        usersRecyclerAdapter = new UsersRecyclerAdapter(listUsers);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerViewUsers.setLayoutManager(mLayoutManager);
+        recyclerViewUsers.setItemAnimator(new DefaultItemAnimator());
+        recyclerViewUsers.setHasFixedSize(true);
+        recyclerViewUsers.setAdapter(usersRecyclerAdapter);
+        databaseHelper = new DatabaseHelper(activity);
+
+        String emailFromIntent = getIntent().getStringExtra("EMAIL");
+        textViewName.setText(emailFromIntent);
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                listUsers.clear();
+                listUsers.addAll(databaseHelper.getAllUser());
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                usersRecyclerAdapter.notifyDataSetChanged();
+            }
+        }.execute();
+
+    }
+}
